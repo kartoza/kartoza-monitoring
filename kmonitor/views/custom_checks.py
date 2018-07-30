4e6cd483981e82213dc780764923034da5861137
sonlinux <alison.mukoma@yahoo.com>
2018-07-30 14:24:05 +0200

    add base dashboard for import

--- kmonitor/views/custom_checks.py
+++ kmonitor/views/custom_checks.py
@@ -1,18 +1,3 @@
-# coding: utf-8
-__author__ = 'Alison Mukoma <alison@kartoza.com>'
-__copyright__ = 'kartoza.com'
-
 # custom kmonitor scripts
-from prometheus_client import Counter
 
-landing_page_counter = Counter(
-                'open_landing_page',
-                'User open the landing page')
-
-
-class PrometheusCounter:
-
-    @staticmethod
-    def increase_landing_page_view():
-        landing_page_counter.inc()
 
